e9cf06dfd7ec6d9c4f2a652e2f39f84e6a024fbf
YisusLuligo <jesusa.luligop@uqvirtual.edu.co>
2025-11-09 16:29:00 -0500

    Add files via upload

---
+++ proyecto_estructuras/src/main/java/edu/universidad/estructuras/proyecto_estructuras/RecuperarPasswordController.java
@@ -0,0 +1,65 @@
+package edu.universidad.estructuras.proyecto_estructuras;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class RecuperarPasswordController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button backToLoginButton;
+
+    @FXML
+    private Label confirmationMessage1;
+
+    @FXML
+    private VBox confirmationView;
+
+    @FXML
+    private Hyperlink loginLink;
+
+    @FXML
+    private VBox requestPasswordView;
+
+    @FXML
+    private Button sendInstructionsButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    void handleBack(ActionEvent event) throws IOException {
+        requestPasswordView.setVisible(true);
+        confirmationView.setVisible(false);
+        HelloApplication.setRoot("login");
+
+    }
+
+    @FXML
+    void handleBackToLogin(ActionEvent event) throws IOException {
+        requestPasswordView.setVisible(true);
+        confirmationView.setVisible(false);
+        HelloApplication.setRoot("login");
+    }
+
+    @FXML
+    void handleSendInstructions(ActionEvent event) throws IOException {
+        if( !usernameField.getText().equals("") ){
+            requestPasswordView.setVisible(false);
+            confirmationView.setVisible(true);
+            usernameLabel.setText(usernameField.getText());
+        }
+    }
+}
---
+++ proyecto_estructuras/src/main/java/edu/universidad/estructuras/proyecto_estructuras/model/Usuario.java
@@ -0,0 +1,116 @@
+package edu.universidad.estructuras.proyecto_estructuras.model;
+
+import java.util.*;
+
+public class Usuario {
+    private final String username;
+    private String password;
+    private String nombre;
+    private String correo;
+    private TipoUsuario tipo;
+    private LinkedList<Cancion> listaFavoritos;
+
+
+    public Usuario(String username, String password,
+                   String nombre, String correo, TipoUsuario tipo) {
+        this.username = username;
+        this.password = password;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.listaFavoritos = new LinkedList<>();
+        this.correo = correo;
+    }
+
+
+    public Usuario(String username, String password, String nombre, String correo) {
+        this(username, password, nombre, correo, TipoUsuario.USUARIO);
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
+
+    public LinkedList<Cancion> getListaFavoritos() {
+        return listaFavoritos;
+    }
+
+
+
+    public boolean agregarFavorito(Cancion cancion) {
+        if (!listaFavoritos.contains(cancion)) {
+            listaFavoritos.add(cancion);
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean eliminarFavorito(Cancion cancion) {
+        return listaFavoritos.remove(cancion);
+    }
+
+
+    public boolean esFavorita(Cancion cancion) {
+        return listaFavoritos.contains(cancion);
+    }
+
+
+    public boolean esAdministrador() {
+        return tipo == TipoUsuario.ADMINISTRADOR;
+    }
+
+
+    public int getCantidadFavoritos() {
+        return listaFavoritos.size();
+    }
+
+    /*
+      hashCode basado en username.
+      Lo usamos para meter usuarios en HashMap.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+     // Dos usuarios son iguales si tienen el mismo username.
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Usuario otro = (Usuario) obj;
+        return username.equals(otro.username);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (@%s) - %d favoritos",
+                nombre, username, listaFavoritos.size());
+    }
+}
