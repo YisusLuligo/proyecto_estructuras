8f6cc89725f13b6878cb2c1429c02d2895a5fee5
YisusLuligo <jesusa.luligop@uqvirtual.edu.co>
2025-11-09 21:33:28 -0500

    Delete proyecto_estructuras directory

--- proyecto_estructuras/src/main/java/edu/universidad/estructuras/proyecto_estructuras/exception/CancionNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package edu.universidad.estructuras.proyecto_estructuras.exception;
-
-public class CancionNoEncontradaException extends Exception {
-    public CancionNoEncontradaException(String mensaje) {
-        super(mensaje);
-    }
-}
